29dfc24ef5164ab3c4a4aaa174f40478a85aeb71
Soemsak Lpps <soemsakplus@gmail.com>
2020-03-04 14:13:34 +0700

    save

--- Example/NustFirstTest/pages/test/index.vue
+++ Example/NustFirstTest/pages/test/index.vue
@@ -3,37 +3,71 @@
     <h1>{{ name }}</h1>
     <a v-bind:href ="url">Link to Google</a>
     <div v-html="htlmData"></div>
-    <logo />
+    <div v-if="isShow">
+      <logo />
+    </div>
+    <input v-model="message" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+    <button @click="buttonClicked">OK</button>
+    <button @click="showHideClicked">Show Hide</button>
+    <div>
+      <ChartBar />
+    </div>
+    <div>
+      <ChartDoughnut />
+    </div>
+    <div>
+      <ChartLine />
+    </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import ChartBar from "@/components/chart-bar";
+import ChartDoughnut from "@/components/chart-doughnut";
+import ChartLine from "@/components/chart-line";
 
 export default {
   components: {
-    Logo
+    Logo,
+    ChartBar,
+    ChartDoughnut,
+    ChartLine
   },
   data: () => ({
     title: "Breeze Test",
     info: "info",
     url: "https://google.com",
     htlmData: "<h1>Hi HTML</h1>",
-    name: ""
+    name: "",
+    message: "",
+    isShow: false
   }),
 	mounted () {
-		this.initData ()
+		//this.initData ()
 	},
   methods: {
     test() {
       return 'test test test'
     },
-    initData () {
+    initData() {
       this.$axios.$get('http://localhost:3000/test1')
         .then( response => (
           this.name = response.name
         ))
-		}
+    },
+    buttonClicked() {
+      this.message = ""
+      this.initData()
+    },
+    showHideClicked() {
+      if (this.isShow) {
+        this.isShow = false
+      } else {
+        this.isShow = true
+      }
+    }
   }
 }
 </script>
